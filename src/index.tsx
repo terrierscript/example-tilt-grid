21913c96fbeb4443a9bfba6e531556634d06be89
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-27 19:21:03 +0900

    remove

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
-import React, { useLayoutEffect } from "react"
+import React from "react"
 import { render } from "react-dom"
-// import styled, { StyleSheetManager } from "styled-components"
-import styled from "@emotion/styled"
-import { default as sstyled } from "styled-components"
-import { css } from "@emotion/core"
 import { App } from "./app/App"
 
 render(<App />, document.querySelector("#root"))
