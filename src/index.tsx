77063d31f63ac8ce44bcfe33c8179fede560b1a4
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-27 12:18:45 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { render } from "react-dom"
 // import styled, { StyleSheetManager } from "styled-components"
 import styled from "@emotion/styled"
@@ -6,7 +6,7 @@ import { CacheProvider } from "@emotion/core"
 import createCache from "@emotion/cache"
 
 export const myCache = createCache({
-  key: "my-prefix-key",
+  // key: "my-prefix-key",
   prefix: false
 })
 const NUM = 10
@@ -19,6 +19,7 @@ const Grid = styled.div`
   grid-template-rows: repeat(${NUM}, ${SIZE}px);
   grid-template-columns: repeat(${NUM}, ${SIZE}px);
   grid-gap: 1em;
+  translate: 0.5s;
 `
 
 const ItemBg = styled.div`
@@ -29,18 +30,50 @@ const ItemBg = styled.div`
     background: blue;
   }
 `
-const ItemPeace = styled.div`
-  grid-row: ${(props) => props.x};
-  grid-column: ${(props) => props.y};
-  background: green;
-  z-index: 2;
+
+const PEACE_COLOR = "rgba(0,255,0,0.6)"
+
+const Flat = styled.div`
+  position: absolute;
   height: ${SIZE}px;
   width: ${SIZE}px;
-  transform: rotateX(90deg) 
-    /* translateZ(${-SIZE}px)  */
-    /* translateY(${SIZE / 2}px); */
-  ;
+  background: ${PEACE_COLOR};
+  border: 1px solid black;
 `
+const PeacePos = styled.div<{ x: number; y: number }>`
+  grid-row: ${(props) => props.x};
+  grid-column: ${(props) => props.y};
+`
+const Front = styled(Flat)`
+  transform: rotateX(90deg) translateZ(${-SIZE / 2}px) translateY(${SIZE / 2}px);
+`
+const Back = styled(Flat)`
+  transform: rotateX(90deg) translateZ(${SIZE / 2}px) translateY(${SIZE / 2}px);
+`
+const Left = styled(Flat)`
+  transform: rotateY(90deg) translateZ(${SIZE / 2}px) translateX(-${SIZE / 2}px);
+`
+const Right = styled(Flat)`
+  transform: rotateY(90deg) translateZ(-${SIZE / 2}px)
+    translateX(-${SIZE / 2}px);
+`
+const Bottom = styled(Flat)``
+const Top = styled(Flat)`
+  transform: translateZ(${SIZE}px);
+`
+
+const ItemPeace = (props) => {
+  return (
+    <PeacePos {...props}>
+      <Bottom />
+      <Front />
+      <Back />
+      <Left />
+      <Right />
+      <Top />
+    </PeacePos>
+  )
+}
 
 const Item = () => {
   return (
@@ -56,33 +89,54 @@ const TilteCamera = styled.div`
   transform-style: preserve-3d;
 `
 const Center = styled.div`
-  /* transform: translateX(calc(50% / 1.41)); */
+  transform: translateX(calc(50% / 1.41));
   /* transform-style: preserve-3d; */
 `
-const App = () => {
+
+const Camera = ({ children }) => {
+  return (
+    <Center>
+      <TilteCamera>{children}</TilteCamera>
+    </Center>
+  )
+}
+
+const clamp = (x, lower, upper) => {
+  return Math.min(Math.max(x, lower), upper)
+}
+const useKeys = () => {
   const [x, setX] = useState(3)
   const [y, setY] = useState(3)
+  useEffect(() => {
+    setInterval(() => {
+      const d = 0.5 - Math.random() > 0 ? 1 : -1
+      Math.random() > 0.5
+        ? setX((v) => clamp(v + d, 0, NUM))
+        : setY((v) => clamp(v + d, 0, NUM))
+    }, 1000)
+  }, [])
+  console.log()
+  return [x, y]
+}
+const App = () => {
+  const [x, y] = useKeys()
   return (
-    <>
-      {/* <StyleSheetManager stylisOptions={{ prefix: false }}> */}
-      <CacheProvider value={myCache}>
-        <Center>
-          <TilteCamera>
-            <Grid>
-              {Array(NUM * NUM)
-                .fill(null)
-                .map((_, i) => (
-                  <Item key={i} />
-                ))}
-            </Grid>
-            <Grid>
-              <ItemPeace x={x} y={y} />
-            </Grid>
-          </TilteCamera>
-        </Center>
-      </CacheProvider>
-      {/* </StyleSheetManager> */}
-    </>
+    // {/* <StyleSheetManager stylisOptions={{ prefix: false }}> */}
+    <CacheProvider value={myCache}>
+      <Camera>
+        <Grid>
+          {Array(NUM * NUM)
+            .fill(null)
+            .map((_, i) => (
+              <Item key={i} />
+            ))}
+        </Grid>
+        <Grid>
+          <ItemPeace x={x} y={y} />
+        </Grid>
+      </Camera>
+    </CacheProvider>
+    // {/* </StyleSheetManager> */}
   )
 }
 
