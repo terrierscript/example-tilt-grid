01d059d8c66f36b06bf1879c77bea3e2c9650721
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-27 09:57:48 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import { render } from "react-dom"
-import styled from "styled-components"
-
+import styled, { StyleSheetManager } from 'styled-components'
 const NUM = 10
 const SIZE = 40
+
 const Grid = styled.div`
+  position: absolute;
+
+  background: rgba(0,100,100,0.5);
   display: grid;
   grid-template-rows: repeat(${NUM}, ${SIZE}px);
   grid-template-columns: repeat(${NUM}, ${SIZE}px);
   grid-gap: 1em;
 `
+
 const ItemBg = styled.div`
   background: rgba(255,0,0,0.6);
   height: ${SIZE}px;
@@ -19,18 +23,22 @@ const ItemBg = styled.div`
   }
 `
 const ItemPeace = styled.div`
+  grid-row: ${(props) => props.x};
+  grid-column: ${(props) => props.y};
   background: green;
-  height: ${SIZE/2}px;
+  z-index: 2;
+  height: ${SIZE}px;
   width: ${SIZE}px;
-  transform: rotateX(90deg) translateZ(${-SIZE/4}px)  translateY(${SIZE/4}px);
-
+  transform: rotateX(90deg) 
+    /* translateZ(${-SIZE}px)  */
+    /* translateY(${SIZE/2}px); */
+  ;
 `
 
 const Item = () => {
   return (
     <>
       <ItemBg>
-        <ItemPeace />
       </ItemBg>
     </>
   )
@@ -46,12 +54,23 @@ const Center = styled.div`
   transform-style: preserve-3d;
 `
 const App = () => {
+  const [x, setX] = useState(3)
+  const [y, setY] = useState(3)
   return (
     <Center>
-      <TilteCamera>
-        <Grid>{Array(NUM * NUM).fill(<Item />)}</Grid>
-      </TilteCamera>
-    </Center>
+      <StyleSheetManager stylisOptions={{ prefix: false }}>
+        <TilteCamera>
+          <Grid>{Array(NUM * NUM)
+            .fill(null)
+            .map((_,i) => <Item key={i} />)}
+          </Grid>
+          <Grid>
+          <ItemPeace x={x} y={y} />
+
+          </Grid>
+        </TilteCamera>
+     </StyleSheetManager>
+      </Center>
   )
 }
 
