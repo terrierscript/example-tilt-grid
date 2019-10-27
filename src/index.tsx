d334806d60052716e4d8fd2d60753ed6d8aaded3
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-27 11:27:59 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react"
 import { render } from "react-dom"
-import styled, { StyleSheetManager } from 'styled-components'
+// import styled, { StyleSheetManager } from "styled-components"
+import styled from "@emotion/styled"
+import { CacheProvider } from "@emotion/core"
+import createCache from "@emotion/cache"
+
+export const myCache = createCache({
+  key: "my-prefix-key",
+  prefix: false
+})
 const NUM = 10
 const SIZE = 40
 
 const Grid = styled.div`
   position: absolute;
-
-  background: rgba(0,100,100,0.5);
+  background: rgba(0, 100, 100, 0.5);
   display: grid;
   grid-template-rows: repeat(${NUM}, ${SIZE}px);
   grid-template-columns: repeat(${NUM}, ${SIZE}px);
@@ -15,10 +22,10 @@ const Grid = styled.div`
 `
 
 const ItemBg = styled.div`
-  background: rgba(255,0,0,0.6);
+  background: rgba(255, 0, 0, 0.6);
   height: ${SIZE}px;
   width: ${SIZE}px;
-  :hover{
+  :hover {
     background: blue;
   }
 `
@@ -31,46 +38,51 @@ const ItemPeace = styled.div`
   width: ${SIZE}px;
   transform: rotateX(90deg) 
     /* translateZ(${-SIZE}px)  */
-    /* translateY(${SIZE/2}px); */
+    /* translateY(${SIZE / 2}px); */
   ;
 `
 
 const Item = () => {
   return (
     <>
-      <ItemBg>
-      </ItemBg>
+      <ItemBg></ItemBg>
     </>
   )
 }
 
 const TilteCamera = styled.div`
   width: max-content;
-  transform: rotateX(60deg) rotateZ(45deg);
+  transform: rotateX(60deg) rotateZ(45deg) translateX(calc(100%));
   transform-style: preserve-3d;
 `
 const Center = styled.div`
-  transform: translateX(calc(50% / 1.41));
-  transform-style: preserve-3d;
+  /* transform: translateX(calc(50% / 1.41)); */
+  /* transform-style: preserve-3d; */
 `
 const App = () => {
   const [x, setX] = useState(3)
   const [y, setY] = useState(3)
   return (
-    <Center>
-      <StyleSheetManager stylisOptions={{ prefix: false }}>
-        <TilteCamera>
-          <Grid>{Array(NUM * NUM)
-            .fill(null)
-            .map((_,i) => <Item key={i} />)}
-          </Grid>
-          <Grid>
-          <ItemPeace x={x} y={y} />
-
-          </Grid>
-        </TilteCamera>
-     </StyleSheetManager>
-      </Center>
+    <>
+      {/* <StyleSheetManager stylisOptions={{ prefix: false }}> */}
+      <CacheProvider value={myCache}>
+        <Center>
+          <TilteCamera>
+            <Grid>
+              {Array(NUM * NUM)
+                .fill(null)
+                .map((_, i) => (
+                  <Item key={i} />
+                ))}
+            </Grid>
+            <Grid>
+              <ItemPeace x={x} y={y} />
+            </Grid>
+          </TilteCamera>
+        </Center>
+      </CacheProvider>
+      {/* </StyleSheetManager> */}
+    </>
   )
 }
 
